22de4218abc797147474e84f8e89da9f1f9e2576
qiu <qiuzhongqiang@users.noreply.github.com>
2022-03-10 18:45:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >>feeds.conf.default
-echo 'src-git libdocker https://github.com/lisaac/luci-lib-docker' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git dockerce https://github.com/docker/docker-ce' >>feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
+
+sed -i '$a src-git libdocker https://github.com/lisaac/luci-lib-docker' feeds.conf.default
+
+sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
+
+sed -i '$a src-git dockerce https://github.com/docker/docker-ce' feeds.conf.default
