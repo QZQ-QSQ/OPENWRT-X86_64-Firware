de0f202f4466681493c53f437beb4e75f91ac00f
qiu <qiuzhongqiang@users.noreply.github.com>
2022-03-11 22:08:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+#### 修改主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='X86'' package/lean/default-settings/files/qiu-default-settings
+#### 稳定版修改R21xxx+自己的名字
+sed -i 's/R22.2.2/R22.3.15/g' package/lean/default-settings/files/qiu-default-settings
